14ed2b5cdebc94b50283747a3c126b4a1c6277c7
flashback2k14 <yeahdev@gmail.com>
2023-02-23 23:03:18 +0100

    Testing: add swagger

---
+++ scripts/swagger.js
@@ -0,0 +1,68 @@
+const swaggerAutogen = require('swagger-autogen')({ openapi: '3.0.0' });
+const pkg = require('../package.json');
+
+const doc = {
+  info: {
+    title: 'Yeah! Time tracking API',
+    version: pkg.version,
+    description: pkg.description,
+  },
+  host: 'localhost:3000',
+  basePath: '/',
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  securityDefinitions: {
+    accessHeader: {
+      type: 'apiKey',
+      in: 'header',
+      name: 'x-access-token',
+      description: 'Settings: API access token as header: Get token from `/sign-in` endpoint',
+    },
+    accessBody: {
+      type: 'apiKey',
+      in: 'body',
+      name: 'token',
+      description: 'Settings: API access token as request body: Get token from `/sign-in` endpoint',
+    },
+    accessQuery: {
+      type: 'apiKey',
+      in: 'query',
+      name: 'token',
+      description:
+        'Settings: API access token as request URL query param: Get token from `/sign-in` endpoint',
+    },
+    apiHeader: {
+      type: 'apiKey',
+      in: 'header',
+      name: 'x-api-token',
+      description: 'API access token as header: Get token from `/authentication` endpoint',
+    },
+    apiBody: {
+      type: 'apiKey',
+      in: 'body',
+      name: 'apitoken',
+      description: 'API access token as request body: Get token from `/authentication` endpoint',
+    },
+    apiQuery: {
+      type: 'apiKey',
+      in: 'query',
+      name: 'apitoken',
+      description:
+        'API access token as request URL query param: Get token from `/authentication` endpoint',
+    },
+  },
+};
+
+const outputFile = './static/swagger-openapi.json';
+const endpointsFiles = [
+  './src/index.ts',
+  './src/domains/auth/auth.router.ts',
+  './src/domains/settings/authentication/authentication.router.ts',
+  './src/domains/settings/action-groups/action-group.router.ts',
+  './src/domains/settings/actions/action.router.ts',
+  './src/domains/tasks/active/active-tasks.router.ts',
+  './src/domains/tasks/history/history-tasks.router.ts',
+];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
