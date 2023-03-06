410a8b7d2384951e7df503afdab60b601b9bd1d1
flashback2k14 <yeahdev@gmail.com>
2023-03-06 12:49:00 +0100

    Testing: add github action to run tests on CI

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
-// !!!! https://dev.to/nathan_sheryak/how-to-test-a-typescript-express-api-with-jest-for-dummies-like-me-4epd
-
 // TODO: add components and refs to docs
-// https://github.com/davibaltar/swagger-autogen
+//  https://github.com/davibaltar/swagger-autogen
+// TODO: add cors and helmet
 
 import app from './app/app';
 
